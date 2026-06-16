c34cf32f522b56481d9f93d9fe2c980f355f2b5c
Claire Masiga <ClaireMasiga22@gmail.com>
2026-06-16 13:13:13 +0300

    Remove custom distDir to fix Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // This tells Next.js to build to a folder called 'dist' 
-  // instead of '.next', which prevents the file locking error.
-  distDir: 'dist', 
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
